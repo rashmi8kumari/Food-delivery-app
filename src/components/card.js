b3b506efb437632dad79a409ea43d4a70d4763fe
Rashmi Kumari <135432782+rashmi8kumari@users.noreply.github.com>
2024-04-25 21:33:19 +0530

    admin

---
+++ src/components/card.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function card() {
+  return (
+    <div>
+      <div>
+        <div
+          className="card mt-3"
+          style={{ width: "18rem", maxHeight: "360px" }}
+        >
+          <img src="https://media.istockphoto.com/id/1457979959/photo/snack-junk-fast-food-on-table-in-restaurant-soup-sauce-ornament-grill-hamburger-french-fries.jpg?s=1024x1024&w=is&k=20&c=S3F3bV7Qv_mGWHGRjK2Xcx33qSODKaXbbhcZyrJBbBc=" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">This is some Important text.</p>
+            <div className="container w-100">
+              <select className="m-2 h-100  bg-success">
+                {Array.from(Array(6), (e, i) => {
+                  return (
+                    <option key={i + 1} value={i + 1}>
+                      {" "}
+                      {i + 1}
+                    </option>
+                  );
+                })}
+              </select>
+              <select className="m-2 h-100  bg-success rounded">
+                <option value="half">Half</option>
+                <option value="full">Full</option>
+              </select>
+              <div className="d-inline h-100 fs-5">Total Price</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
